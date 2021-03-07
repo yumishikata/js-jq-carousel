e8459dab8a684523d081401e8452c70953b67924
Yumi Shikata <yumishika17@gmail.com>
2021-03-07 20:43:51 +0100

    Freccine tastiera

--- main.js
+++ main.js
@@ -32,6 +32,45 @@ $('.prev i').click(function() {
     }
 });
 
+//FRECCINE TASTIERA
+$(document).keydown(function(e) {
+    switch (e.which) { 
+        case 39: 
+            if( $('img.active').hasClass("last")) {
+            $('img.first').addClass("active");
+            $('img.last').removeClass("active");
+            } else {
+            $('img.active').next().addClass("active");
+            $('img.active').prev().removeClass("active");
+            } 
+            if( $('.fa-circle.active').hasClass("last")) {
+                $('.fa-circle.first').addClass("active");
+                $('.fa-circle.last').removeClass("active");
+            } else {
+            $('.fa-circle.active').next().addClass("active");
+            $('.fa-circle.active').prev().removeClass("active");
+            }
+            break;
+        case 37:
+            if( $('img.active').hasClass("first")) {
+                $('img.last').addClass("active");
+                $('img.first').removeClass("active");
+            } else {
+            $('img.active').prev().addClass("active");
+            $('img.active').next().removeClass("active");
+            }
+            if( $('.fa-circle.active').hasClass("first")) {
+                $('.fa-circle.last').addClass("active");
+                $('.fa-circle.first').removeClass("active");
+            } else {
+            $('.fa-circle.active').prev().addClass("active");
+            $('.fa-circle.active').next().removeClass("active");
+            }
+            break;
+}
+});
+
+//PALLINI
 $('.fa-circle.uno').click(function() {
     $('.fa-circle').removeClass("active");
     $(this).addClass("active");
